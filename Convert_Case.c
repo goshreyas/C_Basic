b612b35c0346d0db7bbd6754eb5bba366c89c30a
Shreyas <shreyas.ranjan@gmail.com>
2020-01-11 18:32:51 +0530

    Update Convert_Case.c

--- Convert_Case.c
+++ Convert_Case.c
@@ -1,12 +1,12 @@
 Ref: https://www.geeksforgeeks.org/c-operators-question-15/
 
-#define VAL 32
+#define VAL 32 // 2^5 : 2<<5
 
 char toUpper(char ch) {
-  return (ch & ~VAL);  // Reset 7th bit.
+  return (ch & ~VAL);  // Reset 5th bit.
 }
 
 
 char toLower(char ch) {
-  return (ch | VAL);   // Set 7th bit.
+  return (ch | VAL);   // Set 5th bit.
 }
