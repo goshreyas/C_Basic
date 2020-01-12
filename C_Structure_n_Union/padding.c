34d899e87326ff536cf2225ad11c27257ea5ae53
Shreyas <shreyas.ranjan@gmail.com>
2020-01-12 17:57:55 +0530

    Create padding.c

---
+++ C_Structure_n_Union/padding.c
@@ -0,0 +1,5 @@
+Structure Member Alignment, Padding and Data Packing:
+  https://www.geeksforgeeks.org/structure-member-alignment-padding-and-data-packing/
+How to avoid Structure Padding in C:
+  https://www.geeksforgeeks.org/how-to-avoid-structure-padding-in-c/
+
