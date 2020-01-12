cca3e16eaff97b141be89ab7da9bd33a86c07ce2
Shreyas <shreyas.ranjan@gmail.com>
2020-01-12 22:27:08 +0530

    Update Basic.c

--- Basic.c
+++ Basic.c
@@ -1,3 +1,15 @@
 
+-----------------------------------------------------------------
 function-pointer-in-c @ https://www.geeksforgeeks.org/function-pointer-in-c/
 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+int (*p)[5];  Vs int *p[5];
+int (*p)[5];  // p is a pointer to an array of 5 integers
+int *p[5];  // p is array of 5 pointers to integers. 
+
+Ref: https://www.geeksforgeeks.org/c-c-quiz-106-question-2/
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  
+  
+  
