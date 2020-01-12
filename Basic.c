3fe9438fabb4045b1d23d14eaf2bede6e1a3c175
Shreyas <shreyas.ranjan@gmail.com>
2020-01-13 04:26:44 +0530

    Update Basic.c

--- Basic.c
+++ Basic.c
@@ -10,6 +10,13 @@ int *p[5];  // p is array of 5 pointers to integers.
 
 Ref: https://www.geeksforgeeks.org/c-c-quiz-106-question-2/
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
-  
+Return from function:
+int [] sum(int a, int b); // In-valid:
+  - It will result in compile error.
+  - A function can’t have an explicit array as return type.
+int * sum(int a, int b);  // Valid
+
+Ref: https://www.geeksforgeeks.org/c-c-quiz-107-question-5/
+
+-----------------------------------------------------------------
   
