f6589143362f0374217e3ec918aeb10170f8ac92
Shreyas <shreyas.ranjan@gmail.com>
2020-01-12 13:57:28 +0530

    Create declare_pointer_to_a_node.c

---
+++ C_Structure_n_Union/declare_pointer_to_a_node.c
@@ -0,0 +1,26 @@
+Ref: https://www.geeksforgeeks.org/c-c-quiz-108-question-3/
+
+==================================================================
+/* First declaration */
+typedef struct node 
+{ 
+ int data; 
+ struct node *nextPtr; 
+}* NODEPTR;  // Valid
+
+NODEPTR n1;
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/* Second declaration */
+struct node 
+{ 
+ int data; 
+ struct node * nextPtr; 
+}; 
+typedef struct node * NODEPTR; // Valid
+
+NODEPTR n1;
+
+==================================================================
+
+Explanation: Either of the above declarations can be used for “NODEPTR nodePtr;”. In fact, first one is the compact form of second one.
