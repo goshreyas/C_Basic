9c6b57ea26e3018df83af6719befb79921c5bd79
Shreyas <shreyas.ranjan@gmail.com>
2020-01-13 06:32:13 +0530

    Update Pointer_to_array.c

--- C_Arrays/Pointer_to_array.c
+++ C_Arrays/Pointer_to_array.c
@@ -3,19 +3,19 @@
 # include <stdio.h>
 int main ()
 {
-		int i, j;
-		char a[2][3] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}};
-		printf("a: %p, *a: %p, a[0][1]: %c\n", a, *a, *(*(a+0)+1));
-		char b[3][2];
-		printf("b: %p, *b: %p, &b: %p\n", b, *b, &b);
-		printf("b+1: %p, *b+1: %p, &b+1: %p\n", b+1, *b+1, &b+1);
-		//char *p = *b;
-		char *p = (char *)b;
-		for (i = 0; i < 2; i++) {
-			for (j = 0; j < 3; j++) {
-			*(p + 2*j + i) = a [i] [j];
-			}
+	int i, j;
+	char a[2][3] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}};
+	printf("a: %p, *a: %p, a[0][1]: %c\n", a, *a, *(*(a+0)+1));
+	char b[3][2];
+	printf("b: %p, *b: %p, &b: %p\n", b, *b, &b);
+	printf("b+1: %p, *b+1: %p, &b+1: %p\n", b+1, *b+1, &b+1);
+	//char *p = *b;  // Valid
+	char *p = (char *)b;
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < 3; j++) {
+			*(p + 2*j + i) = a[i][j];
 		}
+	}
 }
 
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
