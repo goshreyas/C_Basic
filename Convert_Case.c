9b9b8952857b8304d59e27f8c6b9d2e4676905c7
Shreyas <shreyas.ranjan@gmail.com>
2020-01-15 04:58:43 +0530

    Update Convert_Case.c

--- Convert_Case.c
+++ Convert_Case.c
@@ -6,7 +6,30 @@ char toUpper(char ch) {
   return (ch & ~VAL);  // Reset 5th bit.
 }
 
-
 char toLower(char ch) {
   return (ch | VAL);   // Set 5th bit.
 }
+
+
+
+============================================================
+// https://www.geeksforgeeks.org/c-string-question-10/
+#include <stdio.h>
+
+void my_toUpper(char* str, int index)
+{
+	*(str + index) &= ~32;
+}
+
+int main()
+{
+	char arr[10] = "geeksquiz";
+	my_toUpper(arr, 0);
+	my_toUpper(arr, 5);
+	printf("%s", arr);
+	return 0;
+}
+++++++++++++++++++++++++++++++
+Output:
+GeeksQuiz
+============================================================
