a62b978fcd3f68026097ed6b9d11d25eda616845
Shreyas <shreyas.ranjan@gmail.com>
2020-11-02 09:41:07 +0530

    Create macro.c

---
+++ macro.c
@@ -0,0 +1,8 @@
+code the macro offsetof
+https://www.geeksforgeeks.org/the-offsetof-macro/
+
+
+
+code the macro sizeof
+https://cs-fundamentals.com/tech-interview/c/implement-sizeof-operator-in-c
+https://www.geeksforgeeks.org/implement-your-own-sizeof/
