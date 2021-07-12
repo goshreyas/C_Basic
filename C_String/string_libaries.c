06283f5e1c8eb6d717f8c4f37e06cd7d67d63b38
Shreyas <shreyas.ranjan@gmail.com>
2021-07-13 01:19:23 +0530

    Update string_libaries.c

--- C_String/string_libaries.c
+++ C_String/string_libaries.c
@@ -46,11 +46,30 @@ void *memcpy(void *dest, const void *src, size_t count)
 
 
 
+void *memmove(void *dest, const void *src, size_t count)
+{
+	char *tmp;
+	const char *s;
 
+	if (dest <= src) {
+		tmp = dest;
+		s = src;
+		while (count--)
+			*tmp++ = *s++;
+	} else {
+		tmp = dest;
+		tmp += count;
+		s = src;
+		s += count;
+		while (count--)
+			*--tmp = *--s;
+	}
+	return dest;
+}
 
+SRC: https://elixir.bootlin.com/linux/latest/source/lib/string.c#L902
 
-
-
+//================================================================================================================
 
 
 
